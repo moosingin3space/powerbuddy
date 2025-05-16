097c7eb8081685fcf5cb88a2a3cd6a3e134a342b
Nathan Moos <moosingin3space@gmail.com>
2025-05-16 16:10:50 -0700

    feat: add Meraki API client to worker src

---
+++ worker/src/meraki_api.ts
@@ -0,0 +1,5 @@
+// Using the documentation for the Meraki API located at
+// https://developer.cisco.com/meraki/api-v1/create-device-sensor-command/
+// write a function that takes an object of the shape { power: boolean } and
+// enables/disables downstream power on an MT40, then waits for the sensor to
+// report power being in the desired state, using exponential backoff. AI!
