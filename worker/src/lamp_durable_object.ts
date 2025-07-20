7b5583d798dae2a8775db979026d4df33fc1e6ec
Nathan Moos <moosingin3space@gmail.com>
2025-07-20 13:54:26 -0700

    feat: add persisted snapshots

--- worker/src/lamp_durable_object.ts
+++ worker/src/lamp_durable_object.ts
@@ -1,4 +1,4 @@
-import { type ActorRefFrom, createActor } from 'xstate';
+import { type ActorRefFrom, createActor, SnapshotFrom } from 'xstate';
 import { createLampMachine } from './lamp_fsm.ts';
 import * as api from './meraki_api.ts';
 import { DurableObject } from 'cloudflare:workers';
@@ -10,9 +10,11 @@ function isLateNight(env: Env): boolean {
 	return now.hour < 2 || now.hour > 22;
 }
 
+type LampMachine = ReturnType<typeof createLampMachine>;
+
 /** This class implements a lamp controlled with an MT40. */
 export class LampDurableObject extends DurableObject {
-	#fsm: ActorRefFrom<ReturnType<typeof createLampMachine>>;
+	#fsm: ActorRefFrom<LampMachine> | null = null;
 
 	constructor(ctx: DurableObjectState, env: Env) {
 		super(ctx, env);
@@ -29,7 +31,36 @@ export class LampDurableObject extends DurableObject {
 			isLateNight: () => isLateNight(env),
 		});
 
-		this.#fsm = createActor(machine);
+		ctx.blockConcurrencyWhile(async () => {
+			await this.restoreOrCreate(machine);
+		});
+	}
+
+	// Sets up a subscription for this finite state machine,
+	// which will persist the state to storage whenever it changes.
+	subscribeToSnapshot() {
+		if (!this.#fsm) {
+			throw new Error('FSM not initialized');
+		}
+		this.#fsm.subscribe(async () => {
+			const snapshot = this.#fsm ? this.#fsm.getPersistedSnapshot() : null;
+			await this.ctx.storage.put('snapshot', snapshot);
+		});
+	}
+
+	async restoreOrCreate(machine: LampMachine) {
+		if (this.#fsm) {
+			throw new Error('actor already initialized');
+		}
+
+		const snapshot = await this.ctx.storage.get<SnapshotFrom<LampMachine>>('snapshot');
+		if (snapshot) {
+			this.#fsm = createActor(machine, { snapshot });
+		} else {
+			this.#fsm = createActor(machine);
+		}
+		this.#fsm.start();
+		this.subscribeToSnapshot();
 	}
 
 	myDeviceDetected(connected: boolean) {
@@ -43,6 +74,8 @@ export class LampDurableObject extends DurableObject {
 
 	// Manual override control stub
 	setManualOverride(action: 'on' | 'off') {
-		this.#fsm.send({ type: `manual_override_${action}` });
+		if (this.#fsm) {
+			this.#fsm.send({ type: `manual_override_${action}` });
+		}
 	}
 }
